2cb6c90ce8de8c4397520526f0a765ad757e1353
Lachlan Cotter <lach@lachlancotter.com>
2023-06-10 14:22:09 -0300

    Add view configuration

--- lib/lucid/view.rb
+++ lib/lucid/view.rb
@@ -3,42 +3,138 @@ require "lucid/route"
 require "lucid/link"
 
 module Lucid
+  #
+  # Base class for Lucid views. Defines a DSL for constructing a
+  # view with links, actions, data sources and routes.
+  #
   class View
     class << self
-      def state (&block) end
+      # ===================================================== #
+      #    State Definition
+      # ===================================================== #
 
+      #
+      # Defines the state attributes and validation rules
+      # for this view component. View state encapsulates the
+      # parameters that are exposed to the client.
+      #
+      def state (&block)
+        nil
+      end
+
+      # ===================================================== #
+      #    Dependency Injection
+      # ===================================================== #
+
+      #
+      # The class config block defines the configuration options
+      # and defaults that are available to instances.
+      #
+      def config (&block)
+        Docile.dsl_eval(ConfigDef.new(self), &block).build
+      end
+
+      #
+      # Configuration options DSL.
+      #
+      class ConfigDef
+        def initialize (klass)
+          @klass = klass
+        end
+
+        def option (name, default)
+          @klass.define_method(name) do
+            @config.fetch(name, default)
+          end
+        end
+
+        def build
+          @klass
+        end
+      end
+
+      # ===================================================== #
+      #    Routes
+      # ===================================================== #
+
+      #
+      # The route block defines the path components and params
+      # to project from the view state.
+      #
       def route (&block)
-        @routes = Route::Map.build(route_config, &block)
+        define_method(:routes) do
+          @routes ||= Route::Map.build(routes_config, &block)
+        end
       end
 
-      def route_config
-        {
-           path_root: "/counter"
-        }
-      end
+      # ===================================================== #
+      #    Links
+      # ===================================================== #
 
+      #
+      # Defines a link with a name and a block that mutates
+      # the view state, producing the state to link to.
+      #
       def link (name, &block)
         define_method(name) do
           new_state = state.mutate(&block)
           Link.new(new_state, routes)
         end
       end
+
+      # ===================================================== #
+      #    Actions
+      # ===================================================== #
+
     end
 
-    def initialize (state = {})
+    config do
+      option :path_root, "/"
+    end
+
+    def initialize (state = {}, &config)
       @state  = State.new(state)
+      @config = Configure.new(&config).to_h
       @links  = SimpleDelegator.new(self)
+    end
+
+    #
+    # Provides a scope to set configuration options during
+    # initialization, which can then be encapsulated in the
+    # view instance.
+    #
+    class Configure
+      def initialize
+        @store = {}
+        yield self if block_given?
+      end
+
+      def to_h
+        @store
+      end
+
+      private
+
+      def method_missing(symbol, *args)
+        if symbol.to_s =~ /=$/
+          @store[symbol.to_s.gsub(/=$/, "").to_sym] = args.first
+        else
+          super
+        end
+      end
     end
 
     attr_reader :state
     attr_reader :links
 
-    def routes
-      self.class.instance_variable_get(:@routes)
-    end
-
     def to_s
       render
+    end
+
+    private
+
+    def routes_config
+      { path_root: path_root }
     end
   end
 end
