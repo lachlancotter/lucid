544c4f67b0e64d511d9e7852a48973d23e2d4f27
Lachlan Cotter <lach@lachlancotter.com>
2025-03-23 16:40:35 -0300

    Refactor guard template

--- lib/lucid/component/guarded.rb
+++ lib/lucid/component/guarded.rb
@@ -1,13 +1,21 @@
 module Lucid
-  module Component
+  module Component 
     #
-    # Guard conditions for components. Prevents access to the Render object if
-    # the guard condition evaluates to Deny so that the component cannot render.
+    # Provides guard expressions for components. Guards are conditions that must
+    # be met in order for the component to render. If the guard condition is not
+    # met, the component will return a special DENIED_TEMPLATE instead of the
+    # normal template.
+    # 
+    # You can override the DENIED_TEMPLATE content by defining a template with the same
+    # name in your component.
     #
     module Guarded
+      DENIED_TEMPLATE = :__denied__
+      
       def self.included(base)
         base.extend(ClassMethods)
-        # base.prepend(RenderOverride)
+        base.template(DENIED_TEMPLATE) { text "Denied" }
+        base.prepend(TemplateOverride)
       end
 
       def guards
@@ -21,60 +29,15 @@ module Lucid
       def permitted?
         !denied?
       end
-
-      #
-      # Guard condition is still denied after all patrols have been attempted.
-      #
-      # class MaxPatrolsExceeded < StandardError
-      #   def initialize
-      #     super("Max patrols exceeded")
-      #   end
-      # end
-      #
-      # MAX_PATROLS = 3
-      #
-      # #
-      # # Check all the guard conditions in this component and all subcomponents.
-      # # If a guard condition denies access to the component, notify the component
-      # # via the Denied event so that components can take action. Then check the
-      # # guard conditions again up to the maximum number of patrols.
-      # #
-      # def check_guards (max = MAX_PATROLS, &block)
-      #   max.times do
-      #     if_denied { |result| notify Guard::Denied.new(result: result) }
-      #     if_permitted { return block.call if block_given? }
-      #   end
-      #   raise MaxPatrolsExceeded
-      # end
-
-      #
-      # Call the block if any guard condition in this component or a subcomponent
-      # denies access to the component. Always returns nil.
-      #
-      # def if_denied (&block)
-      #   guards.each do |guard|
-      #     guard.if_denied { |result| return block.call(result) }
-      #   end
-      #   subcomponents.values.each do |sub|
-      #     Match.on(sub) do
-      #       type(Component::Base) do |singleton|
-      #         singleton.if_denied { |result| return block.call(result) }
-      #       end
-      #       type(Enumerable) do |enum|
-      #         enum.each { |sub| sub.if_denied { |result| return block.call(result) } }
-      #       end
-      #     end
-      #   end
-      #   nil
-      # end
-
-      #
-      # Call the block if all guard conditions in this component and all subcomponents
-      # permit access to the component. Always returns nil.
-      #
-      def if_permitted (&block)
-        block.call if permitted?
-        nil
+      
+      module TemplateOverride
+        def template (name = Rendering::BASE_TEMPLATE)
+          if denied?
+            self.class.template(DENIED_TEMPLATE).bind(self)
+          else
+            super
+          end
+        end
       end
 
       #
--- spec/lucid/component/guarded_spec.rb
+++ spec/lucid/component/guarded_spec.rb
@@ -7,39 +7,42 @@ module Lucid
           component = Class.new(Base).new({})
           expect(component.denied?).to be_falsey
           expect(component.permitted?).to be_truthy
-          expect { component.render_full }.not_to raise_error
+          expect(component.template.render).to eq("Base")
+        end
+      end
+
+      context "permit" do
+        it "allows access to the template" do
+          component = Class.new(Base) { guard { Permit } }.new({})
+          expect(component.denied?).to be_falsey
+          expect(component.permitted?).to be_truthy
+          expect(component.template.render).to eq("Base")
+        end
+      end
+
+      context "deny" do
+        it "denies access to the template" do
+          component = Class.new(Base) do
+            guard { Deny }
+            element { h1 { text "Exposed!" } }
+          end.new({})
+          expect(component.denied?).to be_truthy
+          expect(component.permitted?).to be_falsey
+          expect(component.template.render).to eq("Denied")
         end
       end
 
       context "invalid result" do
         it "raises an exception on render" do
           component = Class.new(Base) { guard { "foo" } }.new({})
-          expect { component.render_full }.to raise_error(Guard::Invalid)
+          expect { component.template }.to raise_error(Guard::Invalid)
         end
       end
 
       context "guard exception" do
         it "raises an exception on render" do
           component = Class.new(Base) { guard { raise "foo" } }.new({})
-          expect { component.render_full }.to raise_error(RuntimeError)
-        end
-      end
-
-      context "permit" do
-        it "allows the component to render" do
-          component = Class.new(Base) { guard { Permit } }.new({})
-          expect(component.denied?).to be_falsey
-          expect(component.permitted?).to be_truthy
-          expect(component.render_full).to be_a(String)
-        end
-      end
-
-      context "deny" do
-        it "raises an exception on render" do
-          component = Class.new(Base) { guard { Deny } }.new({})
-          expect(component.denied?).to be_truthy
-          expect(component.permitted?).to be_falsey
-          expect { component.render_full }.to raise_error(Guard::Violation)
+          expect { component.template }.to raise_error(RuntimeError)
         end
       end
 
@@ -53,80 +56,48 @@ module Lucid
         end
       end
 
-      describe "#if_denied" do
-        context "no guards" do
-          it "does not call the block" do
-            component = Class.new(Base).new({})
-            expect { |b| component.if_denied(&b) }.not_to yield_control
-          end
-        end
 
-        context "when permitted" do
-          it "does not call the block" do
-            component = Class.new(Base) { guard { Permit } }.new({})
-            expect { |b| component.if_denied(&b) }.not_to yield_control
-          end
-        end
-        
-        context "when denied" do
-          it "yields the result to the block" do
-            component = Class.new(Base) { guard { Deny } }.new({})
-            expect { |b| component.if_denied(&b) }.to yield_with_args(Guard::Deny)
-          end
-        end
-
-        context "when denied in subcomponent" do
-          it "yields the denied result to the block" do
-            component = Class.new(Base) do
-              nest(:foo) { Class.new(Base) { guard { Deny } } }
-            end.new({})
-            expect { |b| component.foo.if_denied(&b) }.to yield_with_args(Guard::Deny)
-            expect { |b| component.if_denied(&b) }.to yield_with_args(Guard::Deny)
-          end
-        end
-      end
-
-      describe "#check_guards" do
-        context "no guards" do
-          it "returns Permit" do
-            component = Class.new(Base).new({})
-            expect(component.check_guards).to eq(Permit)
-          end
-        end
-
-        context "unresolved guard" do
-          it "raises max patrols exception" do
-            component = Class.new(Base) { guard { Deny } }.new({})
-            expect { component.check_guards }.to raise_error(Guarded::MaxPatrolsExceeded)
-          end
-        end
-
-        context "resolved guard" do
-          it "returns Permit" do
-            component = Class.new(Base) do
-              param :foo, Types.bool.default(false)
-              guard { |foo| foo ? Permit : Deny }
-              on(Guard::Denied) { update(foo: true) }
-            end.new
-            expect do
-              expect(component.check_guards).to eq(Permit)
-            end.not_to raise_error
-          end
-        end
-
-        context "unresolved nested guard" do
-          it "raises åx check_guardss exception" do
-            component = Class.new(Base) do
-              param :foo, Types.bool.default(false)
-              on(Guard::Denied) { update(foo: true) }
-              nest :bar do
-                Class.new(Base) { guard { Deny } }
-              end
-            end.new
-            expect { component.check_guards }.to raise_error(Guarded::MaxPatrolsExceeded)
-          end
-        end
-      end
+      # describe "#check_guards" do
+      #   context "no guards" do
+      #     it "returns Permit" do
+      #       component = Class.new(Base).new({})
+      #       expect(component.check_guards).to eq(Permit)
+      #     end
+      #   end
+      #
+      #   context "unresolved guard" do
+      #     it "raises max patrols exception" do
+      #       component = Class.new(Base) { guard { Deny } }.new({})
+      #       expect { component.check_guards }.to raise_error(Guarded::MaxPatrolsExceeded)
+      #     end
+      #   end
+      #
+      #   context "resolved guard" do
+      #     it "returns Permit" do
+      #       component = Class.new(Base) do
+      #         param :foo, Types.bool.default(false)
+      #         guard { |foo| foo ? Permit : Deny }
+      #         on(Guard::Denied) { update(foo: true) }
+      #       end.new
+      #       expect do
+      #         expect(component.check_guards).to eq(Permit)
+      #       end.not_to raise_error
+      #     end
+      #   end
+      #
+      #   context "unresolved nested guard" do
+      #     it "raises åx check_guardss exception" do
+      #       component = Class.new(Base) do
+      #         param :foo, Types.bool.default(false)
+      #         on(Guard::Denied) { update(foo: true) }
+      #         nest :bar do
+      #           Class.new(Base) { guard { Deny } }
+      #         end
+      #       end.new
+      #       expect { component.check_guards }.to raise_error(Guarded::MaxPatrolsExceeded)
+      #     end
+      #   end
+      # end
 
     end
   end
