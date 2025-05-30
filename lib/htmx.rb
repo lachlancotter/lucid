6f2c831dca1d4b5c565d6c14e74118f9c1caebe4
Lachlan Cotter <lach@lachlancotter.com>
2025-05-30 00:13:09 -0300

    Add HTMX morph extension

--- lib/htmx.rb
+++ lib/htmx.rb
@@ -2,18 +2,29 @@
 # DSL for generating HTMX attributes.
 #
 class HTMX < Hash
-  LIB = {
-     src:         "https://unpkg.com/htmx.org@1.9.11",
-     integrity:   "sha384-0gxUXCCR8yv9FM2b+U3FDbsKthCI66oH5IA9fHppQq9DDMHuMauqq1ZHBpJxQ0J0",
+  LIB           = {
+     src:         "https://unpkg.com/htmx.org@2.0.4",
+     integrity:   "sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+",
+     crossorigin: "anonymous"
+  }
+  IDIOMORPH     = {
+     src:         "https://unpkg.com/idiomorph@0.7.3",
+     integrity:   "sha384-JcorokHTL/m+D6ZHe2+yFVQopVwZ+91GxAPDyEZ6/A/OEPGEx1+MeNSe2OGvoRS9",
+     crossorigin: "anonymous"
+  }
+  IDIOMORPH_EXT = {
+     src:         "https://unpkg.com/idiomorph@0.7.3/dist/idiomorph-ext.min.js",
+     integrity:   "sha384-szktAZju9fwY15dZ6D2FKFN4eZoltuXiHStNDJWK9+FARrxJtquql828JzikODob",
      crossorigin: "anonymous"
   }
 
   def self.boost
-    self[boost: true]
+    self[boost: true, ext: "morph"]
   end
 
-  def self.oob (outerHTML: nil, beforeend: nil, afterbegin: nil, delete: nil)
+  def self.oob (morphOuterHTML: nil, outerHTML: nil, beforeend: nil, afterbegin: nil, delete: nil)
     return self["swap-oob": "outerHTML:##{outerHTML}"] unless outerHTML.nil?
+    return self["swap-oob": "morph:outerHTML:##{morphOuterHTML}"] unless morphOuterHTML.nil?
     return self["swap-oob": "beforeend:##{beforeend}"] unless beforeend.nil?
     return self["swap-oob": "afterbegin:##{afterbegin}"] unless afterbegin.nil?
     return self["swap-oob": "delete:##{delete}"] unless delete.nil?
