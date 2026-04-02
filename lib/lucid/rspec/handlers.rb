35318617590ae8fb12212f0f86b1e22d175c3298
Lachlan Cotter <lach@lachlancotter.com>
2026-04-01 22:49:46 -0300

    Check RSpec is defined before use

--- lib/lucid/rspec/handlers.rb
+++ lib/lucid/rspec/handlers.rb
@@ -4,27 +4,29 @@ module Lucid
       #
       # Specify that a handler adopts a policy class.
       # 
-      ::RSpec::Matchers.define :adopt_policy do |policy_class|
-        match do |handler_class|
-          instantiate(handler_class).policy.class == policy_class
-        end
+      if defined?(::RSpec)
+        ::RSpec::Matchers.define :adopt_policy do |policy_class|
+          match do |handler_class|
+            instantiate(handler_class).policy.class == policy_class
+          end
 
-        failure_message do |actual|
-          "expected #{actual} to adopt #{policy_class}, but it's actual policy is #{instantiate(actual).policy}"
-        end
+          failure_message do |actual|
+            "expected #{actual} to adopt #{policy_class}, but it's actual policy is #{instantiate(actual).policy}"
+          end
 
-        failure_message_when_negated do |actual|
-          "expected #{actual} not to adopt #{policy_class}, but it does"
-        end
+          failure_message_when_negated do |actual|
+            "expected #{actual} not to adopt #{policy_class}, but it does"
+          end
 
-        description do
-          "adopt #{policy_class}"
-        end
+          description do
+            "adopt #{policy_class}"
+          end
 
-        def instantiate (handler_class)
-          @handler_instance ||= begin
-            message = double("Message")
-            handler_class.new(message, container)
+          def instantiate (handler_class)
+            @handler_instance ||= begin
+              message = double("Message")
+              handler_class.new(message, container)
+            end
           end
         end
       end
